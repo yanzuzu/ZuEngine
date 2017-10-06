2b07583e8e8c25239281586a1af43af5f0bc45d5
SamLo <yanzuzu0723@gmail.com>
2017-10-07 00:38:31 +0800

    add controller UI.

---
+++ ZuEngine/Assets/Game/scripts/UI/ControllerView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerView : MonoBehaviour 
+{
+	[SerializeField]
+	private Button m_leftBtn;
+	[SerializeField]
+	private Button m_rightBtn;
+	[SerializeField]
+	private Button m_gasBtn;
+	[SerializeField]
+	private Button m_backBtn;
+
+	void Start()
+	{
+	}
+}
