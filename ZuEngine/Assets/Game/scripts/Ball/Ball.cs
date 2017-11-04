63c5cbc2015bfcb82dde16c18a7aeb21de8b21b4
SamLo <yanzuzu0723@gmail.com>
2017-11-05 00:54:40 +0800

    add the ball assets and attach functon by the vehicle.

---
+++ ZuEngine/Assets/Game/scripts/Ball/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZuEngine.Utility;
+
+public class Ball : MonoBehaviour 
+{
+	private Transform m_attachTarget;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	void Update()
+	{
+		if ( null == m_attachTarget )
+		{
+			return;
+		}
+
+		transform.position = m_attachTarget.position + new Vector3(0,2f,0) + m_attachTarget.forward * 6f;
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if ( other.gameObject.layer != LayerMask.NameToLayer ("Vehicle") )
+		{
+			return;
+		}
+
+		m_attachTarget = other.gameObject.transform;
+	}
+}
