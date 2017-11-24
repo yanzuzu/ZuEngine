2cc16be7bfa6aed46fa80f112dcde172548112d7
SamLo <yanzuzu0723@gmail.com>
2017-11-25 00:03:55 +0800

    add test txt

---
+++ ZuEngine/Assets/test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int a = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
